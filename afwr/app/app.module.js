72737d18de2b7896b2b3cc880fb56ae0d5b1fdfa
Robert Mayer <rwlmayer@gmail.com>
2017-01-16 22:37:31 +0100

    added navbar component to app module

--- afwr/app/app.module.js
+++ afwr/app/app.module.js
@@ -11,6 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_js_1 = require("./app.component.js");
+var navbar_component_js_1 = require("./Components/Navbar/navbar.component.js");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -19,7 +20,7 @@ var AppModule = (function () {
 AppModule = __decorate([
     core_1.NgModule({
         imports: [platform_browser_1.BrowserModule],
-        declarations: [app_component_js_1.AppComponent],
+        declarations: [app_component_js_1.AppComponent, navbar_component_js_1.NavbarComponent],
         bootstrap: [app_component_js_1.AppComponent]
     }),
     __metadata("design:paramtypes", [])
--- afwr/app/app.module.ts
+++ afwr/app/app.module.ts
@@ -1,11 +1,12 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component.js';
+import { AppComponent } from './app.component.js';
+import { NavbarComponent } from './Components/Navbar/navbar.component.js';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+    imports: [BrowserModule],
+    declarations: [AppComponent, NavbarComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
