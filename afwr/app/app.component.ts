00cb688fedcd38380ca1bda86c9618292b602436
Robert Mayer <rwlmayer@gmail.com>
2017-01-16 17:05:06 +0100

    Compile on save

--- afwr/app/app.component.ts
+++ afwr/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    name = 'Angular';
+
+    constructor() {
+
+    }
+}
