89b4bf1590f8a1d4048f3fc1e2fc4decaebe0826
Robert Mayer <rwlmayer@gmail.com>
2017-01-17 16:15:58 +0100

    Created Service and implemented into app.component

--- afwr/app/app.component.ts
+++ afwr/app/app.component.ts
@@ -1,13 +1,14 @@
 ﻿import { Component } from '@angular/core';
-
+import { JsonPlaceholderService } from './Services/JsonPlaceholder/jsonplaceholder.service.js';
 @Component({
   selector: 'my-app',
-  templateUrl: '/app/app.component.html'
+  templateUrl: '/app/app.component.html',
+  providers: [JsonPlaceholderService]
 })
 export class AppComponent  {
     name = 'Angular';
 
-    constructor() {
-
+    constructor(private placeholderService: JsonPlaceholderService) {
+        placeholderService.getJson();
     }
 }
