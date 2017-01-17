e162659cfb732e8ddd19935b4a476ca8e6bd736c
Robert Mayer <rwlmayer@gmail.com>
2017-01-17 09:15:17 +0100

    Todo: fix angular-cli

---
+++ afwr/dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,4 @@
+export var environment = {
+    production: true
+};
+//# sourceMappingURL=C:/Private/git/war-room-af/afwr/src/environments/environment.prod.js.map
---
+++ afwr/dist/out-tsc/main.js
@@ -0,0 +1,10 @@
+import './polyfills.ts';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
+if (environment.production) {
+    enableProdMode();
+}
+platformBrowserDynamic().bootstrapModule(AppModule);
+//# sourceMappingURL=C:/Private/git/war-room-af/afwr/src/main.js.map
