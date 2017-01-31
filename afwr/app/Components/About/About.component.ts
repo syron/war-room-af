5491bf998f48bb235da087220de99e35b9b2964a
Robert Mayer <rwlmayer@gmail.com>
2017-01-31 14:19:58 +0100

    Added about component

---
+++ afwr/app/Components/About/About.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+    selector: 'app-about',
+    templateUrl: 'About/About.component.html'
+    //templateUrl: '/app/components/Account/LoginForm.component.html' + fileVersion
+})
+
+
+export class AboutComponent {
+    
+}
+
