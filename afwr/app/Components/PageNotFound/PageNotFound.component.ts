d532f2bf990c532ef7a13fa3ca71a898169b01e8
Robert Mayer <rwlmayer@gmail.com>
2017-01-31 14:13:20 +0100

    Added PageNotFoundComponent

---
+++ afwr/app/Components/PageNotFound/PageNotFound.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+    selector: 'app-loginform',
+    templateUrl: 'PageNotFound/PageNotFound.component.html'
+    //templateUrl: '/app/components/Account/LoginForm.component.html' + fileVersion
+})
+export class PageNotFoundComponent {
+    
+}
+
