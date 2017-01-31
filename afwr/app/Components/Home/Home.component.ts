77f33dea690445f3e5bc1ca2ad313b14bd83f51d
Robert Mayer <rwlmayer@gmail.com>
2017-01-31 14:18:03 +0100

    Added home component

---
+++ afwr/app/Components/Home/Home.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'Home/Home.component.html'
+})
+
+export class HomeComponent {
+    
+}
+
